c9d56896ff5e244f81519d8bf9d8f96b9159e77b
ankur <mrmonk.learn@gmail.com>
2023-06-09 15:41:36 +0530

    new temp

---
+++ main.js
@@ -0,0 +1,55 @@
+console.log("nice");
+
+var water = [
+        {
+            No: 1,
+            Name: "IKSA",
+            Qty: "1000 ML",
+            Mrp: 125
+        },
+        {
+            No: 2,
+            Name: "HIMALAYAN",
+            Qty: "1000 ML",
+            Mrp: 125
+
+        },
+        {
+            No: 3,
+            Name: "TUBIG",
+            Qty: "1000 ML",
+            Mrp: 120
+        },
+        {
+            No: 4,
+            Name: "MOON LIGHT",
+            Qty: "1000 ML",
+            Mrp: 110
+        },
+        {
+            No: 5,
+            Name: "TAJ AQA",
+            Qty: "1000 ML",
+            Mrp: 110
+        },
+        {
+           No: 6,
+           Name: "AGUA",
+           Qty: "1000 ML",
+           Price: 100
+        },
+        {
+            No: 7,
+            Name: "ICE",
+            Qty: "1000 ML",
+            Price: 70
+        }
+        ]
+
+        // jQuery("#popup").empty();
+        // water.forEach(element => {
+        //     jQuery("#popup").append(`<div style="float:left;"><div class="productimg"></div><div style="text-align: center;width: 150px;">${element.Name}</div><div style="text-align: center;width: 150px;" >${element.Mrp}</div></div>`);
+            
+        // });
+
+        // jQuery("#popup").click(()=>{jQuery("#popup").hide()})
